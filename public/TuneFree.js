d55ef5c6f7c34304d933155890554c4bf1faf430
GSQZ <i@sayqz.com>
2024-02-08 02:56:34 +0800

    commit

--- public/TuneFree.js
+++ public/TuneFree.js
@@ -1,8 +1,8 @@
 // CSMusic.js
 module.exports = {
     data: {
-        Name: "Countless Simple Music",
-        Ver: "V2.0.9",
+        Name: "TuneFree",
+        Ver: "1.0.1",
         UpdateTime: "2023-10-28",
         DownloadUrl: "https://cdn.sayqz.com/csm/ver/V2.0.9.exe"
     }
